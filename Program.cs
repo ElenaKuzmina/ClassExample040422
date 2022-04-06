cbe7530a79375aa346c3a8d3c769dd464d004ed1
Elena Kuzmina <kashtan.kuzmin@gmail.com>
2022-04-06 12:33:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample040422
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsAnketa anketa = new clsAnketa(); //объект класса
+            anketa.FIO = "Кузьмина Е.Е.";
+            anketa.Kurs = 5;
+          
+            Console.WriteLine(anketa.PrintInfo());
+
+            clsAnketa anketa1 = new clsAnketa("Немтырева К.А.", 
+                 "Ликино-Дулево", 2, "ИСП.20А", "+79771801465");
+
+            Console.WriteLine(anketa1.PrintInfo());
+
+            //создание массива объектов
+            Console.WriteLine("Введите количество студентов N=");
+            int N = int.Parse(Console.ReadLine());
+
+            clsAnketa[] anketas = new clsAnketa[N];
+            for(int i = 0; i<N; i++)
+            {
+                anketas[i] = new clsAnketa();
+                anketas[i].FIO = Console.ReadLine();
+                anketas[i].Kurs = byte.Parse(Console.ReadLine());
+               
+            }
+            foreach(clsAnketa cls in anketas)
+            {
+                Console.WriteLine(cls.PrintInfo());
+            }
+            Console.ReadKey();
+;        }
+    }
+}
---
+++ clsAnketa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample040422
+{
+    class clsAnketa
+    {//поля класса
+        private string fio;
+        private DateTime birthday;
+        private string adress;
+        private byte kurs;
+        private string group;
+        private string phone;
+        // свойства для доступа к полям
+        public string FIO
+        {
+            get { return  fio; }
+            set { fio = value; }
+        }
+        public byte Kurs
+        {
+            get { return kurs; }
+            set { kurs = value; }
+        }
+        //методы 
+        //конструктор без параметров
+        public clsAnketa()
+        {
+            kurs = 1;
+            group = "ИСП.22А";
+            phone = "+70000000000";
+        }
+        //конструктор с параметрами
+        public clsAnketa(string f,  string ad,
+            byte k, string g, string ph)
+        {
+            fio = f;
+           
+            adress = ad;
+            kurs = k;
+            group = g;
+            phone = ph;
+        }
+        //вывод информации об объекте
+        public string PrintInfo()
+        {
+            return $"Студент: {fio}  " +
+                $"Курс: {kurs}" +
+                $"Группа: {group}" +
+                $"Телефон: {phone}";
+        }
+
+    }
+}
